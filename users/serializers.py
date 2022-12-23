fdaa07b4c331490fd38436bd75bc9281a00f8f3d
VladimirGrebenev <grebenev-81@mail.ru>
2022-12-23 14:25:02 +0300

    Сделал CustomUser. Убрал поле пароль из сериализации аpi.

--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from .models import CustomUser
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'user_name', 'email', 'password', 'date_joined', 'updated', 'is_staff', 'is_active']
+        fields = ['first_name', 'last_name', 'user_name', 'email', 'date_joined', 'updated', 'is_staff', 'is_active']
