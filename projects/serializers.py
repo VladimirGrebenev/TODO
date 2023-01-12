640782bfc386ef68887ac3ade520199e1f4cb02e
VladimirGrebenev <grebenev-81@mail.ru>
2023-01-12 17:11:23 +0300

    dz 3 update

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,16 +1,18 @@
-from rest_framework.serializers import HyperlinkedModelSerializer, StringRelatedField , ModelSerializer
+from rest_framework.serializers import StringRelatedField , ModelSerializer
+from rest_framework.relations import StringRelatedField
 from .models import Project, ToDoTask
 
+
 class ProjectModelSerializer(ModelSerializer):
-    # users = StringRelatedField(many=True)
+    users = StringRelatedField(many=True)
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class ToDoTaskModelSerializer(ModelSerializer):
-    project = ProjectModelSerializer()
-    auther = StringRelatedField()
+    project = StringRelatedField()
+    author = StringRelatedField()
     class Meta:
         model = ToDoTask
         fields = '__all__'
