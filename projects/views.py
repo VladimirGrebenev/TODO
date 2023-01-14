7b6b8e532295fa36438b591b05df618522c056ea
VladimirGrebenev <grebenev-81@mail.ru>
2023-01-14 12:00:50 +0300

    add pagination

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDoTask
 from .serializers import ProjectModelSerializer, ToDoTaskModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
 
 
 # Create your views here.
+class ToDoTaskLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 3
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
@@ -12,3 +17,4 @@ class ProjectModelViewSet(ModelViewSet):
 class ToDoTaskModelViewSet(ModelViewSet):
     queryset = ToDoTask.objects.all()
     serializer_class = ToDoTaskModelSerializer
+    pagination_class = ToDoTaskLimitOffsetPagination
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from .models import CustomUser
 
-class UserModelSerializer(HyperlinkedModelSerializer):
+class UserModelSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'user_name', 'email', 'date_joined', 'updated', 'is_staff', 'is_active']
