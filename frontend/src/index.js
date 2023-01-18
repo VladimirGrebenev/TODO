86783ba5640f1452b22aa989108e91e31e247b79
VladimirGrebenev <grebenev-81@mail.ru>
2023-01-18 17:08:22 +0300

    пофиксил меню - закомментировал  {/*<React.StrictMode>*/}

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
 
-            <React.StrictMode>
+            {/*<React.StrictMode>*/}
                 <App/>
-            </React.StrictMode>
+            {/*</React.StrictMode>*/}
 
     </div>
 );
