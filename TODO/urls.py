fe42affbb0ac81e5db9ea78b28e9618eeeeaf3ff
VladimirGrebenev <grebenev-81@mail.ru>
2023-01-23 11:11:32 +0300

    добавил views.obtain_auth_token

--- TODO/urls.py
+++ TODO/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 from users.views import UserCustomViewSet
 from projects.views import ProjectModelViewSet, ToDoTaskModelViewSet
 
@@ -28,5 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
-
