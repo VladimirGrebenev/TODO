6fbc59e14d69d4a6da2fbcedd4d1096e21b5398a
VladimirGrebenev <grebenev-81@mail.ru>
2023-01-23 10:32:49 +0300

    добавил группы и права пользователей

--- TODO/settings.py
+++ TODO/settings.py
@@ -149,4 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
