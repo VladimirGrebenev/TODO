8fe8bb82a422532e6039ba10eb4076c6dc1d0f80
Vladimir Grebenev <57130730+VladimirGrebenev@users.noreply.github.com>
2023-01-24 09:38:50 +0300

    Lesson 6 (#8)
    * добавил группы и права пользователей
    
    * добавил группы и права пользователей
    
    * добавил добавил авторизацию: базовую, по токену и по сессии
    
    * добавил 'rest_framework.authtoken' в приложения
    
    * добавил views.obtain_auth_token

--- TODO/settings.py
+++ TODO/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
+    'rest_framework.authtoken',
 
     'users',
     'projects',
@@ -146,4 +147,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissions'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
 }
